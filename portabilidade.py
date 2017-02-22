efaffe3d73ecade942051a1781b6796f31aad2f9
Maickel <maickelc@gmail.com>
2017-02-22 08:32:22 -0300

    Update portabilidade.py

--- portabilidade.py
+++ portabilidade.py
@@ -58,7 +58,7 @@ def hello(number=None):
 
 if __name__ == "__main__":
     try:
-        host = '0.0.0.0'
-        app.run(host=host, port=5000,debug=True)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
     except KeyboardInterrupt:
         raise SystemExit
