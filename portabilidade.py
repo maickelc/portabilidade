e5f8ab7684f0842a3bb4404a8fbdff739d497d59
Maickel <maickelc@gmail.com>
2017-02-22 14:12:40 -0300

    Add files via upload

--- portabilidade.py
+++ portabilidade.py
@@ -7,53 +7,61 @@ __date__ = "$09/03/2016 07:41:49$"
 
 from flask import Flask, Response
 import random
-import os
+
 
 operadoras = (
-    '55314',
-    '55324',
-    '55321',
-    '55321',
-    '55312',
     '55301',
-    '55306',
-    '55391',
-    '55393',
-    '55392',
-    '55351',
-    '55377',
-    '55349',
     '55302',
-    '55343',
     '55303',
-    '55375',
-    '55390',
-    '55323',
     '55304',
-    '55341',
-    '55341',
-    '55341',
+    '55305',
+    '55306',
+    '55312',
+    '55314',
+    '55320',
+    '55321',
+    '55323',
+    '55324',
     '55331',
     '55337',
-    '55305',
-    '55320',
+    '55341',
+    '55343',
+    '55349',
+    '55351',
+    '55375',
+    '55377',
+    '55390',
+    '55391',
+    '55392',
+    '55393',
 )
 
-class Oper:
+class Oper(object):
     def __init__(self):
-        self.count = 1
-        self.oper = 1000
+        self.count = 0
+        self.operadoras = operadoras
+        self.iter = self._next()
+        self.current = None
+
+    def _next(self):
+        for x in self.operadoras:
+            yield x
 
     def get(self):
-        return random.choice(operadoras)
+        if self.count == 0:
+            try:
+                self.current = next(self.iter)
+            except StopIteration:
+                self.iter = self._next()
+                self.current = next(self.iter)
+        self.count += 1
+        if self.count == 10:
+            self.count = 0
+        return self.current
 
 app = Flask(__name__)
 
 O = Oper()
-
-@app.route('/')
-def index():
-    return "<h1> Use url/{numerotelefone} </h1>"
 
 @app.route('/<int:number>')
 def hello(number=None):
@@ -62,7 +70,7 @@ def hello(number=None):
 
 if __name__ == "__main__":
     try:
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+        host = '0.0.0.0'
+        app.run(host=host, port=5000,debug=True)
     except KeyboardInterrupt:
         raise SystemExit
